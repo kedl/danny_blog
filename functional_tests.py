a890649a9b193ad438fa922c99bb4bf76aba5383
kedl <kedanlin8121@gmail.com>
2017-09-23 20:09:10 +0800

    add tests

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@
 # @Last Modified time: 2017-09-23 15:33:14
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -19,8 +20,20 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.driver.get(r'http://localhost:8000')
-        self.assertIn('To-Do lists', self.driver.title)
-        #self.fail('Finish the test!')
+        self.assertIn('To-Do', self.driver.title)
+        header_text = self.driver.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.driver.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.driver.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows))
 
 
 if __name__ == '__main__':
