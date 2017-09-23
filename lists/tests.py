8b26ed128e5d282ced1d6bcd4f1e474b07176642
kedl <kedanlin8121@gmail.com>
2017-09-23 16:41:01 +0800

    make unit test

--- lists/tests.py
+++ lists/tests.py
@@ -4,11 +4,14 @@
 # @Date: 2017-09-23 16:12:02
 # @Last Modified by:   Danny
 # @Last Modified time: 2017-09-23 16:12:02
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,12 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: Danny
+# @Date: 2017-09-23 16:18:57
+# @Last Modified by:   Danny
+# @Last Modified time: 2017-09-23 16:18:57
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_page():
+    pass
